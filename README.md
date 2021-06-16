25a1dfa61441064ccd0455518b0dfa7d9aff157d
Ivan Castillo <total180training@gmail.com>
2021-06-16 10:50:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# budget-api
+Budget App API
---
+++ app.js
@@ -0,0 +1,14 @@
+// Dependencies
+const express = require("express");
+const cors = require("cors");
+// Configuration
+const app = express();
+// Middleware
+app.use(express.json());
+app.use(cors());
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to Myra's budgeting app!");
+});
+// Export app
+module.exports = app;
